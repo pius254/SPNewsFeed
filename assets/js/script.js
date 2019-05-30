f57a171bf22454ec043717f626c2e3efbd68a2a2
Pius Muriithi <piusmuriithi1@gmail.com>
2019-05-30 15:28:30 +0300

    Setup project structure

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// Initializing a Vue app
+var growler = new Vue({
+    el: '#app',
+    // Initializing the data property to a Plain Old JavaScript Object (POJO)
+    data: {
+        appName: 'Growler',
+        appLogo: '/assets/imgs/fit.PNG',
+        accentColor: 'accent-color',
+        headers: 'headers' 
+    }
+});
